31acfdd62552eaa59f94c2eb65f248762c616c70
Mith <truonghoangmi@gmail.com>
2016-09-15 11:02:24 +0700

    Add files via upload

--- controllers/home/Index.php
+++ controllers/home/Index.php
@@ -1,1 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mith
+ * Date: 8/22/2016
+ * Time: 11:26 AM
+ */
+class Index extends MY_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper(array('form', 'url'));
+    }
+
+    public function addNews()
+    {
+        $this->render('admin/news_add', 'admin');
+    }
+}
+
 
